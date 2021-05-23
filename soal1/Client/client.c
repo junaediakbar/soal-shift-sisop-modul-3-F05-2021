e97aed0f824d59763a408a4e5325f3e4dc6382d1
junaediakbar <juned.akb@gmail.com>
2021-05-23 19:11:11 +0800

    soal 1 (a dan b)

--- soal1/Client/client.c
+++ soal1/Client/client.c
@@ -1,1 +1,228 @@
-.
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#define PORT 8080
+
+void promptLogReg(int sock);
+void promptChoice(int sock);
+void promptAdd(int sock);
+void printLogRegMsg(int id);
+void send_file(FILE *fp, int sockfd);
+void write_file(int sockfd, char *filename);
+
+int main(int argc, char const *argv[])
+{
+    struct sockaddr_in address;
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char *hello = "Hello from client";
+    char buffer[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    memset(&serv_addr, '0', sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    while (1)
+    {
+        char authMessage[1024] = {0};
+        int valread;
+        printf("---🕓 reading for authmessage \n");
+        valread = read(sock, authMessage, 1024);
+        printf("--- authMessage: %s\n", authMessage);
+
+        if (strcmp(authMessage, "wait") != 0)
+        {
+            promptLogReg(sock);
+        }
+        else
+        {
+            printf("Waiting...\n");
+        }
+    }
+
+    return 0;
+}
+
+// Prompt the client to login or register
+void promptLogReg(int sock)
+{
+    char choice[120], id[100], pass[100];
+    printf("\n=====\nChoose between login or register\n> ");
+    scanf("%s", choice);
+    // TODO REMOVE TEMPORARY STOP
+    if (strcmp(choice, "stop") == 0)
+    {
+        send(sock, "stop", strlen("stop"), 0);
+        exit(0);
+    }
+
+    // if wrong, then restart the prompt.
+    if (!strcmp(choice, "login") == 0 && !strcmp(choice, "register") == 0)
+    {
+        promptLogReg(sock);
+        return;
+    }
+    // Send choice to server
+    send(sock, choice, strlen(choice), 0);
+
+    // Input for login notice
+    printf("Id : ");
+    getchar();
+    scanf("%[^\n]s", id);
+    send(sock, id, strlen(id), 0);
+    printf("Password : ");
+    getchar();
+    scanf("%[^\n]s", pass);
+    send(sock, pass, strlen(pass), 0);
+
+    // AFTER SENDING ID AND PASSWORD, CHECK IF 1 THEN SUCCESSFULL
+    printf("---🕓 reading for logReg message \n");
+    char logRegMsg[1024] = {0};
+    int valread;
+    valread = read(sock, logRegMsg, 1024);
+    printf("--- logRegMst: %s\n", logRegMsg);
+
+    if (strcmp(logRegMsg, "1") == 0)
+    {
+        printf("Login Successful 🥳\n");
+    }
+    else if (strcmp(logRegMsg, "2") == 0)
+    {
+        printf("Register Successful 🥳, you can now login\n");
+        promptLogReg(sock);
+    }
+    else
+    {
+        printf("Login Failed 💔\n");
+        promptLogReg(sock);
+    }
+
+    // Move to choices
+    promptChoice(sock);
+}
+
+void promptChoice(int sock)
+{
+    char choice[120], id[100], pass[100];
+    printf(
+        "\n=====\nChoose [add or stop]\n* use "
+        "full "
+        "path for filepath in add\n> ");
+    scanf("%s", choice);
+
+    if (strcmp(choice, "stop") == 0)
+    {
+        send(sock, "stop", strlen("stop"), 0);
+        exit(0);
+    }
+
+    if (strcmp(choice, "add") == 0)
+    {
+        promptAdd(sock);
+    }
+    // Prompt again
+    promptChoice(sock);
+}
+
+void promptAdd(int sock)
+{
+    send(sock, "add", strlen("add"), 0);
+
+    char publisher[100], tahunPublikasi[100], filename[100];
+    printf("Publisher: ");
+    getchar();
+    scanf("%[^\n]s", publisher);
+    send(sock, publisher, strlen(publisher), 0);
+    printf("Tahun Publikasi: ");
+    getchar();
+    scanf("%[^\n]s", tahunPublikasi);
+    send(sock, tahunPublikasi, strlen(tahunPublikasi), 0);
+    printf("Filepath: ");
+    getchar();
+    scanf("%[^\n]s", filename);
+    send(sock, filename, strlen(filename), 0);
+    sleep(1);
+
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL)
+    {
+        perror("[-]Error in reading file.");
+        exit(1);
+    }
+
+    sleep(1);
+    send_file(fp, sock);
+    printf("🥳 File data sent successfully.\n");
+}
+
+void send_file(FILE *fp, int sockfd)
+{
+    int n;
+    char data[1024] = {0};
+
+    while (fgets(data, 1024, fp) != NULL)
+    {
+        if (send(sockfd, data, sizeof(data), 0) == -1)
+        {
+            perror("[-]Error in sending file.");
+            exit(1);
+        }
+        bzero(data, 1024);
+    }
+
+    // mark last with ending
+    send(sockfd, "stop signal", sizeof("stop signal"), 0);
+}
+
+// Ask for file path first
+void write_file(int sockfd, char *filename)
+{
+    int n;
+    FILE *fp;
+    char buffer[1024];
+
+    printf("--- [write_file()] File to download: %s\n", filename);
+
+    fp = fopen(filename, "w");
+    bzero(buffer, 1024);
+    while (1)
+    {
+        n = recv(sockfd, buffer, 1024, 0);
+
+        // Kalo yang ngirim bukan dari send_file (karena dari function send_file
+        // pasti 1024)
+        if (n != 1024)
+        {
+            break;
+            return;
+        }
+
+        // masukkin ke filenya
+        fprintf(fp, "%s", buffer);
+        bzero(buffer, 1024);
+    }
+    fclose(fp);
+    return;
+}
--- soal1/Server/server.c
+++ soal1/Server/server.c
@@ -1,1 +1,319 @@
-.
+.#include <netinet/in.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#define PORT 8080
+#define SIZE 1024
+
+int connection = 0;
+int currentConnection = 0;
+int queue = 0;
+int id_socket[1000];
+
+void *handleLogReg(void *args);
+void handleSecondPhase(int sock, char *id, char *password);
+void handleStopConnection(int sock);
+void write_file(int sockfd, char *filename);
+void send_file(FILE *fp, int sockfd);
+
+int main(int argc, char const *argv[])
+{
+    // CREATE FILES FOLDER
+    mkdir("FILES", 0777);
+
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+
+    printf("running on port %d\n", PORT);
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt,
+                   sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+
+    pthread_t thread[50];
+
+    while (1)
+    {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                                 (socklen_t *)&addrlen)) < 0)
+        {
+            perror("accept");
+            exit(EXIT_FAILURE);
+        }
+        id_socket[currentConnection] = new_socket;
+
+        if (connection > 0)
+        {
+            printf("📩 sending wait signal to currentConnection: %d\n",
+                   currentConnection);
+            send(id_socket[currentConnection], "wait", strlen("wait"), 1024);
+
+            pthread_create(&thread[currentConnection], NULL, handleLogReg, &new_socket);
+        }
+        else
+        {
+            printf("📩 sending go signal to currentConnection: %d\n",
+                   currentConnection);
+            send(id_socket[currentConnection], "go", strlen("go"), 1024);
+
+            pthread_create(&thread[currentConnection], NULL, handleLogReg, &new_socket);
+        }
+        connection++;
+        currentConnection++;
+    }
+
+    return 0;
+}
+
+void *handleLogReg(void *args)
+{
+    int new_socket = *(int *)args;
+    int valread;
+
+    char buffer[1024] = {0};
+    valread = read(new_socket, buffer, 1024);
+    printf("%s\n", buffer);
+
+    // TODO REMOVE TEMPORARY STOP
+    if (strcmp(buffer, "stop") == 0)
+    {
+        printf(
+            "--- [handleLogReg()] just got stop signal, proceeding "
+            "handleStopConnection()\n");
+        handleStopConnection(new_socket);
+    }
+
+    if (strcmp(buffer, "login") == 0 || strcmp(buffer, "register") == 0)
+    {
+        char id[1024] = {0};
+        valread = read(new_socket, id, 1024);
+        char password[1024] = {0};
+        valread = read(new_socket, password, 1024);
+
+        printf("id: %s, password: %s\n", id, password);
+
+        // if register then write the id and username to the akun.txt
+        if (strcmp(buffer, "register") == 0)
+        {
+            FILE *f;
+            f = fopen("akun.txt", "a+");
+            fprintf(f, "%s:%s\n", id, password);
+
+            char authMessage[100];
+            sprintf(authMessage, "2");
+            send(new_socket, authMessage, strlen(authMessage), 0);
+
+            fclose(f);
+
+            handleLogReg(&new_socket);
+        }
+
+        if (strcmp(buffer, "login") == 0)
+        {
+            FILE *f;
+            f = fopen("akun.txt", "a+");
+            char buffer[1024] = "";
+            int isValid = 0;
+            // While masih ada yang bisa diread, dan selagi masih belom valid (belom
+            // nemu yang id password sama)
+            while (fgets(buffer, 1024, f) != NULL && !isValid)
+            {
+                char compare_id[1025], compare_password[1025];
+
+                // tokenize with ':' as a delimiter
+                char *token = strtok(buffer, ":");
+                strcpy(compare_id, token);
+
+                // get next token until it meets '\n'
+                token = strtok(NULL, "\n");
+                strcpy(compare_password, token);
+
+                if (strcmp(compare_id, id) == 0 && strcmp(compare_password, password) == 0)
+                {
+                    isValid = 1;
+                }
+            }
+
+            char authMessage[500];
+            sprintf(authMessage, "%d", isValid);
+            send(new_socket, authMessage, strlen(authMessage), 0);
+            fclose(f);
+
+            if (isValid)
+            {
+                handleSecondPhase(new_socket, id, password);
+            }
+            else
+            {
+                handleLogReg(&new_socket);
+            }
+        }
+    }
+    else
+    {
+        handleLogReg(&new_socket);
+        pthread_cancel(pthread_self());
+    }
+}
+
+void handleSecondPhase(int sock, char *id, char *password)
+{
+    // WAITING FOR MENU
+    int valread;
+    char buffer[1024] = {0};
+    valread = read(sock, buffer, 1024);
+    printf("--- [handleSecondPhase()] first command: %s\n", buffer);
+
+    // TODO REMOVE TEMPORARY STOP
+    if (strcmp(buffer, "stop") == 0)
+    {
+        printf(
+            "--- [handleSecondPhase()] just got stop signal, proceeding "
+            "handleStopConnection()\n");
+        handleStopConnection(sock);
+    }
+
+    if (strcmp(buffer, "add") == 0)
+    {
+        printf("--- [handleSecondPhase()] add signal\n");
+        printf(
+            "---  [handleSecondPhase()] Preserve id:password for running.log%s:%s\n",
+            id, password);
+
+        // TODO 1. Valread file name dll
+        char publikasi[120] = {0};
+        valread = read(sock, publikasi, 1024);
+        char tahunPublikasi[120] = {0};
+        valread = read(sock, tahunPublikasi, 1024);
+        char filename[120] = {0};
+        valread = read(sock, filename, 1024);
+
+        // TODO 2. Write to files.tsv
+        FILE *fp;
+        fp = fopen("files.tsv", "a+");
+        fprintf(fp, "%s\t%s\t%s\n", publikasi, tahunPublikasi, filename);
+        fclose(fp);
+
+        // TODO 3. Write to running.log
+        fp = fopen("running.log", "a+");
+        // FILES/ get cut off
+        char *filenameWithoutFolder = filename + 6;
+        fprintf(fp, "Tambah: %s (%s:%s)\n", filenameWithoutFolder, id, password);
+        fclose(fp);
+
+        // TODO 4. Write file yang dikirim by line oleh client
+        write_file(sock, filename);
+        printf("🥳 Data written in the file successfully.\n");
+    }
+    // If not the command above, then ask again
+    handleSecondPhase(sock, id, password);
+}
+
+void handleStopConnection(int sock)
+{
+    connection--;
+
+    // ambil connection sebelumnya
+    // currentConnection--;
+    // go to next connection in queue
+    queue++;
+    printf("connection:  %d\n", connection);
+    printf("currentConnection:  %d\n", currentConnection);
+    printf("queue:  %d\n", queue);
+
+    printf("🚥 sending signal to queue: %d is closed to next connection\n", queue);
+    send(id_socket[queue], "go", strlen("go"), 1024);
+
+    // Reset count connection and use the available thread;
+    if (queue == currentConnection)
+    {
+        queue = 0;
+        currentConnection = 0;
+        printf("🕓 currentConnection:  %d\n", currentConnection);
+        printf("🕓 queue:  %d\n", queue);
+    }
+
+    pthread_cancel(pthread_self());
+}
+
+// Ask for file path first
+void write_file(int sockfd, char *filename)
+{
+    int n;
+    FILE *fp;
+    char buffer[SIZE];
+
+    printf("--- [write_file] File to be written in server: %s\n", filename);
+
+    fp = fopen(filename, "w");
+    bzero(buffer, SIZE);
+    while (1)
+    {
+        n = recv(sockfd, buffer, SIZE, 0);
+
+        // Kalo yang ngirim bukan dari send_file (karena dari function send_file
+        // pasti 1024)
+        if (n != 1024)
+        {
+            break;
+            return;
+        }
+
+        // masukkin ke filenya
+        fprintf(fp, "%s", buffer);
+        bzero(buffer, SIZE);
+    }
+    fclose(fp);
+    return;
+}
+
+void send_file(FILE *fp, int sockfd)
+{
+    int n;
+    char data[1024] = {0};
+
+    while (fgets(data, 1024, fp) != NULL)
+    {
+        if (send(sockfd, data, sizeof(data), 0) == -1)
+        {
+            perror("[-]Error in sending file.");
+            exit(1);
+        }
+        bzero(data, 1024);
+    }
+
+    // mark last with ending
+    send(sockfd, "stop signal", sizeof("stop signal"), 0);
+}
